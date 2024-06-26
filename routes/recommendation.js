4829a2a5061a4f9c224ec4f5eb4a921aad281ba2
high5dev <high5dev621@hotmail.com>
2024-06-26 08:58:03 -0400

    recommendations retrived based on primary and secondary concern

--- routes/recommendation.js
+++ routes/recommendation.js
@@ -4,81 +4,120 @@ const Minimalist = require("../model/minimalist");
 const Recommendations = require("../model/recommendations");
 const Product = require("../model/products");
 
-router.post('/', async(req, res) => {
-    try {
-        // const { gender, pregnancy, skin_type, skin_sensitivity, metric, metric_level } = await Minimalist.findById(req.body);
-        const { gender, pregnancy, skin_type, skin_sensitivity, metric, metric_level } = req.body;
-        const recommendation = await Recommendations.find({
-            metric: metric,
-            metric_level: metric_level,
-            gender: gender,
-            pregnancy: pregnancy,
-            skin_type: skin_type,
-            skin_sensitivity: skin_sensitivity
-          });
-          console.log(recommendation[0].cleanser);
-          if (!recommendation) {
-            return res.status(404).json({ message: 'No recommendation found' });
-          }
-
-          const cleanser = await Product.find({
-            title: recommendation[0].cleanser
-          })
-
-          const toner = await Product.find({
-            title: recommendation[0].toner
-          })
-
-          const treatment = await Product.find({
-            title: recommendation[0].treatment
-          })
-
-          const moisturizer = await Product.find({
-            title: recommendation[0].moisturizer
-          })
-
-          const sunscreen = await Product.find({
-            title: recommendation[0].sunscreen
-          })
-
-          const product = {
-            Cleanser: cleanser,
-            Toner: toner,
-            Treatment: treatment,
-            Moisturizer: moisturizer,
-            Sunscreen: sunscreen
-          }
-      
-          // Send the recommendation as a response
-          res.json(product);        
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server error' });
-    }
-})
-
-router.get('/', async(req, res) => {
-    const recommendation = await Recommendations.find();
-    // const { gender, pregnancy, skinType, skinSensitivity } = minimalist;
-    res.status(201).json(recommendation);
-})
-
-router.post('/', async(req, res) => {
-    const recommendation = new Recommendations({
-        gender: req.body.gender,
-        skin_type: req.body.skin_type,
-        skin_sensitivity: req.body.skin_sensitivity,
-        pregnancy: req.body.pregnancy,
-        products: req.body.products,
-        metric: req.body.metric,
-        metric_leve: req.body.metric_leve
+router.post('/', async (req, res) => {
+  try {
+    // const { gender, pregnancy, skin_type, skin_sensitivity, metric, metric_level } = await Minimalist.findById(req.body);
+    const { gender, pregnancy, skin_type, skin_sensitivity, primary_concern, secondary_concern } = req.body;
+    const primaryConcern = primary_concern[0];
+    const secondaryConcern = secondary_concern[0];
+    const primaryRecommendation = await Recommendations.find({
+      metric: Object.keys(primaryConcern)[0],
+      metric_level: Object.values(primaryConcern)[0],
+      gender: gender,
+      pregnancy: pregnancy,
+      skin_type: skin_type,
+      skin_sensitivity: skin_sensitivity
     });
-    try {
-        const newRecommendation = await recommendation.save();
-        res.status(201).json(newRecommendation);
-    } catch (err) {
-        res.status(400).json({msg: err.message})
+
+    const secondaryRecommendation = await Recommendations.find({
+      metric: Object.keys(secondaryConcern)[0],
+      metric_level: Object.values(secondaryConcern)[0],
+      gender: gender,
+      pregnancy: pregnancy,
+      skin_type: skin_type,
+      skin_sensitivity: skin_sensitivity
+    });
+
+    if (!primaryRecommendation && !secondaryRecommendation) {
+      return res.status(404).json({ message: 'No recommendation found' });
     }
+
+    const primaryCleanser = await Product.find({
+      title: primaryRecommendation[0].cleanser
+    })
+
+    const primaryToner = await Product.find({
+      title: primaryRecommendation[0].toner
+    })
+
+    const primaryTreatment = await Product.find({
+      title: primaryRecommendation[0].treatment
+    })
+
+    const primaryMoisturizer = await Product.find({
+      title: primaryRecommendation[0].moisturizer
+    })
+
+    const primarySunscreen = await Product.find({
+      title: primaryRecommendation[0].sunscreen
+    })    
+
+    const secondaryCleanser = await Product.find({
+      title: secondaryRecommendation[0].cleanser
+    })
+
+    const secondaryToner = await Product.find({
+      title: secondaryRecommendation[0].toner
+    })
+
+    const secondaryTreatment = await Product.find({
+      title: secondaryRecommendation[0].treatment
+    })
+
+    const secondaryMoisturizer = await Product.find({
+      title: secondaryRecommendation[0].moisturizer
+    })
+
+    const secondarySunscreen = await Product.find({
+      title: secondaryRecommendation[0].sunscreen
+    })
+
+    const primaryProduct = {
+      Cleanser: primaryCleanser,
+      Toner: primaryToner,
+      Treatment: primaryTreatment,
+      Moisturizer: primaryMoisturizer,
+      Sunscreen: primarySunscreen
+    }    
+
+    const secondaryProduct = {
+      Cleanser: secondaryCleanser,
+      Toner: secondaryToner,
+      Treatment: secondaryTreatment,
+      Moisturizer: secondaryMoisturizer,
+      Sunscreen: secondarySunscreen
+    }
+
+    // Send the recommendation as a response
+    res.json({"primaryProduct": primaryProduct,"secondaryProduct": secondaryProduct});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+})
+
+router.get('/', async (req, res) => {
+  const recommendation = await Recommendations.find();
+  // const { gender, pregnancy, skinType, skinSensitivity } = minimalist;
+  res.status(201).json(recommendation);
+})
+
+router.post('/', async (req, res) => {
+  const recommendation = new Recommendations({
+    gender: req.body.gender,
+    skin_type: req.body.skin_type,
+    skin_sensitivity: req.body.skin_sensitivity,
+    pregnancy: req.body.pregnancy,
+    products: req.body.products,
+    metric: req.body.metric,
+    metric_leve: req.body.metric_leve
+  });
+  try {
+    const newRecommendation = await recommendation.save();
+    res.status(201).json(newRecommendation);
+  } catch (err) {
+    res.status(400).json({ msg: err.message })
+  }
 })
 
 module.exports = router
