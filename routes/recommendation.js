35e0346fde7e5b172ea58bf80d998f3733d2a913
high5dev <high5dev621@hotmail.com>
2024-06-26 08:59:04 -0400

    unused removed

--- routes/recommendation.js
+++ routes/recommendation.js
@@ -98,26 +98,7 @@ router.post('/', async (req, res) => {
 
 router.get('/', async (req, res) => {
   const recommendation = await Recommendations.find();
-  // const { gender, pregnancy, skinType, skinSensitivity } = minimalist;
   res.status(201).json(recommendation);
-})
-
-router.post('/', async (req, res) => {
-  const recommendation = new Recommendations({
-    gender: req.body.gender,
-    skin_type: req.body.skin_type,
-    skin_sensitivity: req.body.skin_sensitivity,
-    pregnancy: req.body.pregnancy,
-    products: req.body.products,
-    metric: req.body.metric,
-    metric_leve: req.body.metric_leve
-  });
-  try {
-    const newRecommendation = await recommendation.save();
-    res.status(201).json(newRecommendation);
-  } catch (err) {
-    res.status(400).json({ msg: err.message })
-  }
 })
 
 module.exports = router
