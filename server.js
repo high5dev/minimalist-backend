474e85272df3c33ff6b307cbb3c698828c7ddf92
high5dev <high5dev621@hotmail.com>
2024-06-28 09:40:21 -0400

    requestpayload too large error fixed

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
+const bodyParser = require('body-parser');
 
 const app = express();
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true});
@@ -19,8 +20,11 @@ const db = mongoose.connection
 db.on('error', (error)=> console.error(error));
 db.once('open', ()=> console.log('connected to db'))
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 const minimalistRouter = require('./routes/minimalist')
 const hautRouter = require('./routes/haut')
 const recommendationRouter = require('./routes/recommendation')
